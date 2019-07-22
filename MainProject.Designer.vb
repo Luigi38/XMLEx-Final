73aefc364c471f0508c70982e23579d2439d1dfb
MineEric64 <besteric40@gmail.com>
2019-07-22 22:59:10 +0900

    V1.0.0.0 - XMLEx Final 추가.

---
+++ MainProject.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainProject
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.XMLTextBox = New System.Windows.Forms.TextBox()
+        Me.lang_ComboBox = New System.Windows.Forms.ComboBox()
+        Me.ExtractButton = New System.Windows.Forms.Button()
+        Me.XMLPathTextBox = New System.Windows.Forms.TextBox()
+        Me.XMLPathButton = New System.Windows.Forms.Button()
+        Me.sfd = New System.Windows.Forms.SaveFileDialog()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.BGW_ExML = New System.ComponentModel.BackgroundWorker()
+        Me.ioGroupBox = New System.Windows.Forms.GroupBox()
+        Me.inRadioButton = New System.Windows.Forms.RadioButton()
+        Me.outRadioButton = New System.Windows.Forms.RadioButton()
+        Me.ioGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'XMLTextBox
+        '
+        Me.XMLTextBox.Location = New System.Drawing.Point(33, 42)
+        Me.XMLTextBox.Name = "XMLTextBox"
+        Me.XMLTextBox.Size = New System.Drawing.Size(406, 21)
+        Me.XMLTextBox.TabIndex = 0
+        '
+        'lang_ComboBox
+        '
+        Me.lang_ComboBox.FormattingEnabled = True
+        Me.lang_ComboBox.Items.AddRange(New Object() {"English (영어)", "Korean (한국어)"})
+        Me.lang_ComboBox.Location = New System.Drawing.Point(350, 232)
+        Me.lang_ComboBox.Name = "lang_ComboBox"
+        Me.lang_ComboBox.Size = New System.Drawing.Size(114, 20)
+        Me.lang_ComboBox.TabIndex = 1
+        Me.lang_ComboBox.Text = "English (영어)"
+        '
+        'ExtractButton
+        '
+        Me.ExtractButton.Font = New System.Drawing.Font("나눔바른고딕OTF", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.ExtractButton.Location = New System.Drawing.Point(33, 84)
+        Me.ExtractButton.Name = "ExtractButton"
+        Me.ExtractButton.Size = New System.Drawing.Size(406, 98)
+        Me.ExtractButton.TabIndex = 2
+        Me.ExtractButton.Text = "Extract!"
+        Me.ExtractButton.UseVisualStyleBackColor = True
+        '
+        'XMLPathTextBox
+        '
+        Me.XMLPathTextBox.Location = New System.Drawing.Point(12, 231)
+        Me.XMLPathTextBox.Name = "XMLPathTextBox"
+        Me.XMLPathTextBox.Size = New System.Drawing.Size(146, 21)
+        Me.XMLPathTextBox.TabIndex = 3
+        Me.XMLPathTextBox.Text = "C:\test.xml"
+        '
+        'XMLPathButton
+        '
+        Me.XMLPathButton.Location = New System.Drawing.Point(164, 231)
+        Me.XMLPathButton.Name = "XMLPathButton"
+        Me.XMLPathButton.Size = New System.Drawing.Size(31, 22)
+        Me.XMLPathButton.TabIndex = 4
+        Me.XMLPathButton.Text = "..."
+        Me.XMLPathButton.UseVisualStyleBackColor = True
+        '
+        'sfd
+        '
+        Me.sfd.DefaultExt = "xml"
+        Me.sfd.Filter = "XML File|*.xml"
+        Me.sfd.Title = "Save The XML File!"
+        '
+        'ofd
+        '
+        Me.ofd.Filter = "XML File|*.xml"
+        Me.ofd.Title = "Open The XML File!"
+        '
+        'BGW_ExML
+        '
+        '
+        'ioGroupBox
+        '
+        Me.ioGroupBox.Controls.Add(Me.outRadioButton)
+        Me.ioGroupBox.Controls.Add(Me.inRadioButton)
+        Me.ioGroupBox.Location = New System.Drawing.Point(201, 188)
+        Me.ioGroupBox.Name = "ioGroupBox"
+        Me.ioGroupBox.Size = New System.Drawing.Size(143, 65)
+        Me.ioGroupBox.TabIndex = 5
+        Me.ioGroupBox.TabStop = False
+        Me.ioGroupBox.Text = "In / Out"
+        '
+        'inRadioButton
+        '
+        Me.inRadioButton.AutoSize = True
+        Me.inRadioButton.Location = New System.Drawing.Point(6, 20)
+        Me.inRadioButton.Name = "inRadioButton"
+        Me.inRadioButton.Size = New System.Drawing.Size(125, 16)
+        Me.inRadioButton.TabIndex = 0
+        Me.inRadioButton.TabStop = True
+        Me.inRadioButton.Text = "Extract 'In Nodes'"
+        Me.inRadioButton.UseVisualStyleBackColor = True
+        '
+        'outRadioButton
+        '
+        Me.outRadioButton.AutoSize = True
+        Me.outRadioButton.Location = New System.Drawing.Point(5, 42)
+        Me.outRadioButton.Name = "outRadioButton"
+        Me.outRadioButton.Size = New System.Drawing.Size(134, 16)
+        Me.outRadioButton.TabIndex = 1
+        Me.outRadioButton.TabStop = True
+        Me.outRadioButton.Text = "Extract 'Out Nodes'"
+        Me.outRadioButton.UseVisualStyleBackColor = True
+        '
+        'MainProject
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(476, 263)
+        Me.Controls.Add(Me.ioGroupBox)
+        Me.Controls.Add(Me.XMLPathButton)
+        Me.Controls.Add(Me.XMLPathTextBox)
+        Me.Controls.Add(Me.ExtractButton)
+        Me.Controls.Add(Me.lang_ComboBox)
+        Me.Controls.Add(Me.XMLTextBox)
+        Me.Name = "MainProject"
+        Me.Text = "XMLEx Final"
+        Me.ioGroupBox.ResumeLayout(False)
+        Me.ioGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents XMLTextBox As TextBox
+    Friend WithEvents lang_ComboBox As ComboBox
+    Friend WithEvents ExtractButton As Button
+    Friend WithEvents XMLPathTextBox As TextBox
+    Friend WithEvents XMLPathButton As Button
+    Friend WithEvents sfd As SaveFileDialog
+    Friend WithEvents ofd As OpenFileDialog
+    Friend WithEvents BGW_ExML As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ioGroupBox As GroupBox
+    Friend WithEvents outRadioButton As RadioButton
+    Friend WithEvents inRadioButton As RadioButton
+End Class
---
+++ MainProject.vb
@@ -0,0 +1,302 @@
+﻿Imports System.ComponentModel
+Imports System.Globalization
+Imports System.IO
+Imports System.Text
+Imports System.Xml
+
+Public Class MainProject
+
+    ''' <summary>
+    ''' Settings XML. (설정 XML.)
+    ''' </summary>
+    Public Shared setxml As New XmlDocument
+    ''' <summary>
+    ''' Settings 'XML'...
+    ''' </summary>
+    Public Shared setaNode As XmlNode
+    ''' <summary>
+    '''Settings XML Path. (설정 XML 경로.)
+    ''' </summary>
+    Public Shared settings_i As String = Application.StartupPath & "\settings.xml"
+
+    ''' <summary>
+    ''' Loading XML Path. (XML 경로.)
+    ''' </summary>
+    Public Shared XML_info As String = String.Empty
+    ''' <summary>
+    ''' Language. (언어.)
+    ''' </summary>
+    Public Shared Lang As CultureInfo
+    ''' <summary>
+    ''' In/Out Nodes. (True: In Nodes, False: Out Nodes)
+    ''' </summary>
+    Public ioNodes As Boolean
+
+    ''' <summary>
+    ''' 따로 저장 여부.
+    ''' </summary>
+    Public DDaroSaved As Boolean = False
+
+    Private trd_FileName As String = String.Empty
+    Private truly_sFileName As String = String.Empty
+
+    Private Sub MainProject_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+            'Exceptions.
+            If Not File.Exists(settings_i) Then
+                Throw New FileNotFoundException("'settings.xml' file doesn't found.")
+                Application.Exit()
+            End If
+
+            setxml.Load(settings_i)
+            setaNode = setxml.SelectSingleNode("/XMLEx-Settings")
+
+            If setaNode IsNot Nothing Then
+
+                XML_info = setaNode.ChildNodes(0).InnerText
+                If Not XML_info = "N/A" Then
+                    XMLPathTextBox.Text = XML_info
+                Else
+                    XML_info = String.Empty
+                    XMLPathTextBox.Text = String.Empty
+                End If
+
+                Select Case setaNode.ChildNodes(1).InnerText
+                    Case "English"
+                        Lang = New CultureInfo("en-US")
+                        lang_ComboBox.Text = "English (영어)"
+                    Case "Korean"
+                        Lang = New CultureInfo("ko-KR")
+                        lang_ComboBox.Text = "Korean (한국어)"
+                    Case Else
+                        lang_ComboBox.Text = "English"
+                        Throw New CultureNotFoundException("We can't translate that culture name.")
+                End Select
+
+                Select Case setaNode.ChildNodes(2).InnerText
+                    Case "InNodes"
+                        ioNodes = True
+                        inRadioButton.Checked = True
+                    Case "OutNodes"
+                        ioNodes = False
+                        outRadioButton.Checked = True
+                End Select
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ExtractButton_Click(sender As Object, e As EventArgs) Handles ExtractButton.Click
+        Try
+
+            If String.IsNullOrEmpty(XML_info) Then Throw New ArgumentNullException("Please select the xml file.")
+            If String.IsNullOrWhiteSpace(XMLTextBox.Text) Then Throw New ArgumentNullException("Please write the Node Name.")
+
+            BGW_ExML.RunWorkerAsync()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ExtractButton_DoubleClick(sender As Object, e As EventArgs) Handles ExtractButton.DoubleClick
+        Try
+
+            If String.IsNullOrEmpty(XML_info) Then Throw New ArgumentNullException("Please select the xml file.")
+            If String.IsNullOrWhiteSpace(XMLTextBox.Text) Then Throw New ArgumentNullException("Please write the Node Name.")
+
+            SaveIn_Ex()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub SaveIn_Ex()
+        Try
+
+            If sfd.ShowDialog() = DialogResult.OK Then
+                trd_FileName = sfd.FileName
+                DDaroSaved = True
+                BGW_ExML.RunWorkerAsync()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BGW_ExML_DoWork(sender As Object, e As DoWorkEventArgs) Handles BGW_ExML.DoWork
+        Try
+
+            Loading.Show()
+            Loading.Text = Me.Text & ": Extracting XML..."
+            Loading.DLb.Text = "Loading The XML File..."
+            Loading.DLb.Refresh()
+            Loading.DPr.Style = ProgressBarStyle.Marquee
+            Loading.DPr.Refresh()
+
+            Dim s As String() = XMLTextBox.Text.Split(" ")
+            Dim doc As New XmlDocument
+            Dim NewElementList As XmlNodeList
+            doc.Load(XML_info)
+            Dim str As String = String.Empty
+            truly_sFileName = Path.GetFileNameWithoutExtension(XML_info)
+
+            If ioNodes Then
+
+                For i As Integer = 0 To s.Count - 1
+                    Dim curri As Integer
+
+                    If i > 0 Then
+                        Dim p As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\tmp_xmlin.xml"
+                        File.WriteAllText(p, str)
+                        doc.Load(p)
+                        str = String.Empty
+                    End If
+
+                    NewElementList = doc.GetElementsByTagName(s(i))
+                    If i = 0 Then
+                        Loading.DPr.Maximum = s.Count * NewElementList.Count
+                        Loading.DPr.Refresh()
+                        Loading.DLb.Text = "Extracting XML Nodes..."
+                        Loading.DLb.Refresh()
+                    End If
+
+                    For Each x As XmlNode In NewElementList
+                        str = str & vbNewLine & x.InnerXml
+                        curri += 1
+                        Loading.DLb.Text = String.Format("Extracting XML Nodes... ({0}/{1})", curri, Loading.DPr.Maximum)
+                        Loading.DLb.Left = Loading.DLb.Left - 50
+                        Loading.DLb.Refresh()
+                        Loading.DPr.Style = ProgressBarStyle.Continuous
+                        Loading.DPr.Value = curri
+                        Loading.DPr.Refresh()
+                    Next
+
+                Next
+
+            ElseIf ioNodes = False Then
+
+                For i As Integer = 0 To s.Count - 1
+
+                    Dim crrui As Integer
+                    str = String.Empty
+                    NewElementList = doc.GetElementsByTagName(s(i))
+                    If i = 0 Then
+                        Loading.DPr.Maximum = s.Count * NewElementList.Count
+                        Loading.DPr.Refresh()
+                        Loading.DLb.Text = "Extracting XML Nodes..."
+                        Loading.DLb.Refresh()
+                    End If
+
+                    For Each x As XmlNode In NewElementList
+                        str = str & vbNewLine & x.OuterXml
+                        crrui += 1
+                        Loading.DLb.Text = String.Format("Extracting XML Nodes... ({0}/{1})", crrui, Loading.DPr.Maximum)
+                        Loading.DLb.Left = Loading.DLb.Left - 50
+                        Loading.DLb.Refresh()
+                        Loading.DPr.Style = ProgressBarStyle.Continuous
+                        Loading.DPr.Value = crrui
+                        Loading.DPr.Refresh()
+                    Next
+
+                    Dim d As Boolean = False
+                    Dim q As Integer = 0
+                    While d = True
+                        q += 1
+                        d = Not File.Exists(String.Format("{0}\{1}_{2}.xml", Application.StartupPath, truly_sFileName, q))
+                    End While
+                    File.WriteAllText(String.Format("{0}\{1}_{2}.xml", Application.StartupPath, truly_sFileName, q + 1), str, Encoding.UTF8)
+
+                Next
+            End If
+
+            If String.IsNullOrWhiteSpace(str) Then
+                Throw New ArgumentNullException("There is no ChildNodes. Please Try again!")
+            End If
+
+            If ioNodes Then 'InNodes 전용 File Write.
+                Dim d As Boolean = False
+                Dim q As Integer = 0
+                While d = True
+                    q += 1
+                    d = Not File.Exists(String.Format("{0}\{1}_{2}.xml", Application.StartupPath, truly_sFileName, q))
+                End While
+                File.WriteAllText(String.Format("{0}\{1}_{2}.xml", Application.StartupPath, truly_sFileName, q + 1), str, Encoding.UTF8)
+            End If
+
+            Loading.Dispose()
+            MessageBox.Show("XML Extracted!", Me.Text & ": Extracted", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub XMLPathButton_Click(sender As Object, e As EventArgs) Handles XMLPathButton.Click
+        Try
+
+            If ofd.ShowDialog() = DialogResult.OK Then
+                XMLPathTextBox.Text = ofd.FileName
+                XML_info = ofd.FileName
+
+                If setaNode IsNot Nothing Then
+                    setaNode.ChildNodes(0).InnerText = XML_info
+                    setxml.Save(settings_i)
+                End If
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Lang_ComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lang_ComboBox.SelectedIndexChanged
+        Try
+
+            If setaNode IsNot Nothing Then
+                Dim s As String = lang_ComboBox.Text
+                Select Case s
+                    Case "English (영어)"
+                        Lang = New CultureInfo("en-US")
+                        setaNode.ChildNodes(1).InnerText = "English"
+                    Case "Korean (한국어)"
+                        Lang = New CultureInfo("ko-KR")
+                        setaNode.ChildNodes(1).InnerText = "Korean"
+                    Case Else
+                        Throw New NullReferenceException("Something went wrong...")
+                End Select
+                setxml.Save(settings_i)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub InRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles inRadioButton.CheckedChanged
+        Try
+
+            If setaNode IsNot Nothing Then
+                If inRadioButton.Checked Then
+                    ioNodes = True
+                    setaNode.ChildNodes(2).InnerText = "InNodes"
+                ElseIf outRadioButton.Checked Then
+                    ioNodes = False
+                    setaNode.ChildNodes(2).InnerText = "OutNodes"
+                Else
+                    Throw New NullReferenceException("Something went wrong...")
+                End If
+                setxml.Save(settings_i)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & ex.StackTrace, Me.Text & ": Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
